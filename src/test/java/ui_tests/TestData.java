59594448bfce88d9425b795f6de5cefa2142dffd
MrBlackk <volodymyr.chornyj@gmail.com>
2014-12-01 23:18:16 +0200

    1.12.14 - replaced MacBook Air 11 in TestData

--- src/test/java/ui_tests/TestData.java
+++ src/test/java/ui_tests/TestData.java
@@ -15,7 +15,8 @@ public class TestData {
 
     //TestData
     public static final String MAC_BOOK_PRO_RETINA_15_CODE = "Z0PU002JE";//Apple MacBook Pro Retina 15 (Z0PU002JE)
-    public static final String MAC_BOOK_AIR_11_CODE = "MD712UA/A"; //Apple MacBook Air 11 (MD712UA/A)
+    //public static final String MAC_BOOK_AIR_11_CODE = "MD712UA/A"; //Apple MacBook Air 11 (MD712UA/A) (товар зник)
+    public static final String MAC_BOOK_AIR_11_CODE = "MD711UA/B"; //Apple MacBook Air 11" (MD711UA/B)
     public static final List<String> MANUFACTURERS_LIST = Arrays.asList("Acer", "Apple", "Asus", "Dell", "Fujitsu", "HP (Hewlett Packard)", "Lenovo", "MSI", "Sony");
 
     //Sort by drop-down
